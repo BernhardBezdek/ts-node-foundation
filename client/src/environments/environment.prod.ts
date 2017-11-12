65c2bf2fea4c7f96f1eab8f703147020d6ec70c0
Bernhard Behrendt <bernhard.behrendt@aoe.com>
2017-11-12 22:30:25 +0100

    [ADD] Feature complete

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,4 +2,9 @@ export const environment = {
     production: true,
     api: '',
     contents: 'assets/content.json',
+
+    dates: {
+        registration: 1510509600000,
+        event: 1511623800000,
+    }
 };
